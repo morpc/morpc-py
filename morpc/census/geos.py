88f8ef7baaee0b8751ede4644b86144a8a66ce4d
Jordan Inskeep <jinskeep@morpc.org>
2025-08-12 08:28:33 -0400

    Begin work on census.geos

---
+++ morpc/census/geos.py
@@ -0,0 +1,9 @@
+import morpc
+
+# TODO (jinskeep_morpc): Develop function for fetching census geographies leveraging scopes
+#   The current geos-lookup workflow is limited by size and scope. This function will be used
+#   to fetch geographies at any scale and scope without the need to store it locally. It is 
+#   limited to census geographies. 
+#   [ ]: Consider storing the data as a remote frictionless resource similar to the acs data class.
+#   [ ]: Define scale and scopes that are used. Possibly lists for benchmarking (i.e. Most populous cities)
+
