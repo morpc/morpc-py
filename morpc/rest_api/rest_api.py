fc90a245a0c48800ec67bed7f29a25758e0816d2
Jordan Inskeep <jinskeep@morpc.org>
2025-08-13 12:27:08 -0400

    improve rest_api.get, include in docs

--- morpc/rest_api/rest_api.py
+++ morpc/rest_api/rest_api.py
@@ -1,4 +1,4 @@
-def schema_from_services_fields(pjson):
+def schema(pjson):
     """Extracts the schema from a JSON object returned by an ArcGIS REST API service.
 
     Parameters:
@@ -32,7 +32,7 @@ def schema_from_services_fields(pjson):
 
     return schema
 
-def get_totalRecordCount(url):
+def totalRecordCount(url):
     """Fetches the total number of records from an ArcGIS REST API service.
     Parameters:
     -----------
@@ -89,19 +89,19 @@ def resource(url):
         "name": re.sub('[:/_ ]', '-', pjson['name']).lower(),
         "format": "json",
         "path": url,
-        "schema": schema_from_services_fields(pjson),
+        "schema": schema(pjson),
         "mediatype": "application/geo+json",
         "_metadata": {
             "type": "arcgis_service",
-            "total_records": get_totalRecordCount(url),
+            "total_records": totalRecordCount(url),
             "max_record_count": pjson['maxRecordCount'],
-            "wkid": pjson['sourceSpatialReference']['wkid']
+            "wkid": pjson['sourceSpatialReference']['wkid'] if 'sourceSpatialReference' in pjson else pjson['spatialReference']['wkid']
         }
     }
 
     return frictionless.Resource(resource)
 
-def esri_wkid_to_epsg(esri_wkid):
+def esri_wkid_to_wkt2(esri_wkid):
     """Converts an ESRI WKID to an EPSG code.
 
     Parameters:
@@ -110,22 +110,49 @@ def esri_wkid_to_epsg(esri_wkid):
         The ESRI WKID to be converted.  
     Returns:
     --------
-    epsg : int
-        The corresponding EPSG code.
+    wkt : int
+        The corresponding Well-Known Text string.
     Example:
     --------
-    >>> epsg = esri_wkid_to_epsg(4326)
-    >>> print(epsg)
-    4326    
+    >>> wkt = esri_wkid_to_wkt2(4326)
+    >>> print(wkt)
+    
 
     """
     import json
     import requests
 
-    r = requests.get(f"https://spatialreference.org/ref/esri/{esri_wkid}/projjson.json")
-    json = r.json()
-    epsg = json['base_crs']['id']['code']
-    return epsg
+    r = requests.get(f"https://spatialreference.org/ref/esri/{esri_wkid}/prettywkt2.txt")
+    wkt = r.text
+    r.close()
+    return wkt
+
+# Depreciated for wkt2
+# def esri_wkid_to_epsg(esri_wkid):
+#     """Converts an ESRI WKID to an EPSG code.
+
+#     Parameters:
+#     -----------
+#     esri_wkid : int
+#         The ESRI WKID to be converted.  
+#     Returns:
+#     --------
+#     epsg : int
+#         The corresponding EPSG code.
+#     Example:
+#     --------
+#     >>> epsg = esri_wkid_to_epsg(4326)
+#     >>> print(epsg)
+#     4326    
+
+#     """
+#     import json
+#     import requests
+
+#     r = requests.get(f"https://spatialreference.org/ref/esri/{esri_wkid}/projjson.json")
+#     json = r.json()
+#     epsg = json['base_crs']['id']['code']
+#     return epsg
 
 def print_bar(i, total):
     """Prints a progress bar to the console.
@@ -139,10 +166,10 @@ def print_bar(i, total):
     """
     from IPython.display import clear_output
 
-    percent = round(i/total * 100, 3)
+    percent = round((i + 1)/total * 100, 3)
     completed = round(percent)
     not_completed = 100-completed
-    bar = f"{i}/{total} |{'|'*completed}{'.'*not_completed}| {percent}%"
+    bar = f"{i+1}/{total} |{'|'*completed}{'.'*not_completed}| {percent}%"
     print(bar)
     clear_output(wait=True)
 
@@ -160,11 +187,17 @@ def get_api_key(path):
     --------
     >>> key = get_api_key('path/to/api_key.txt')
     """
+    import os
+
+    # Verify file exists
+    if not os.path.exists(path):
+        print(f"File does not exist: {path}")
+
     with open(path, 'r') as file:
         key = file.readlines()
     return key[0]
 
-def get(resource_path, field_ids=None, api_key=None):
+def get(resource, field_ids=None, api_key=None, headers = {'User-agent': 'Mozilla/5.0'}, recordcount_override=None):
     """Creates a GeoDataFrame from resource file for an ArcGIS Services. Automatically queries for maxRecordCount and
     iterates over the whole feature layer to return all features. Optional: Filter the results by including a list of field
     IDs.
@@ -173,7 +206,7 @@ def get(resource_path, field_ids=None, api_key=None):
 
     Parameters:
     ------------
-    resource_path : str
+    resource : str
         The path to the resource file, which can be a local file or a URL to an ArcGIS REST API service.
 
     field_ids : list of str
@@ -182,17 +215,17 @@ def get(resource_path, field_ids=None, api_key=None):
     api_key : str, optional
         An API key for accessing the ArcGIS REST API service. If not provided, the function will attempt to access the service without an API key.
 
-    Returns
+    Returns:
     ----------
     gdf : pandas.core.frame.DataFrame
         A GeoPandas GeoDataframe constructed from the GeoJSON requested from the url.
 
     Raises:
-    ------
+    ---------
     RuntimeError: If the provided field_ids are not available in the resource.  
 
     Example:
-    --------
+    ---------
     >>> gdf = get("path/to/resource.json", 
                   field_ids=['OBJECTID', 'NAME'], 
                   api_key=get_api_key('path/to/api_key.txt'))
@@ -201,20 +234,34 @@ def get(resource_path, field_ids=None, api_key=None):
     import requests
     import frictionless
     import geopandas as gpd
+    import pandas as pd
+    from pyproj import CRS
+    import geojson
+    from morpc.rest_api import esri_wkid_to_wkt2, print_bar
 
-    ## Extract important metadata
-    resource = frictionless.Resource(resource_path)
+    # Check if the resource is a string or a frictionless Resource object
+    if isinstance(resource, str):
+        # If it's a string, create a frictionless Resource object from the URL or file path
+        resource = frictionless.Resource(path=resource)
+    elif isinstance(resource, frictionless.Resource):
+        # If it's already a Resource object, use it directly
+        pass    
+
+
+    # Extract metadata from the resource
     url = resource.path
     totalRecordCount = resource.to_dict()['_metadata']['total_records']
-    maxRecordCount = resource.to_dict()['_metadata']['max_record_count']
+    if recordcount_override is not None:
+        maxRecordCount = recordcount_override
+    else:
+        maxRecordCount = resource.to_dict()['_metadata']['max_record_count']
     wkid = resource.to_dict()['_metadata']['wkid']
-    epsg = esri_wkid_to_epsg(wkid) ## Convert ESRI WKID to EPSG code
+    wkt = esri_wkid_to_wkt2(wkid) ## Convert ESRI WKID to wkt2
 
     ## Get field names for filtering fields
     schema = resource.schema
     avail_fields = schema.field_names
 
-    geojson_url = f"{url}/query?outFields=*&where=1%3D1&f=geojson"
     ## Verify fields_ids
     if field_ids != None:
         if not set(field_ids).issubset(avail_fields):
@@ -224,32 +271,34 @@ def get(resource_path, field_ids=None, api_key=None):
             outFields = ",".join(field_ids)
             geojson_url = f"{url}/query?outFields={outFields}&where=1%3D1&f=geojson"
 
+    # Construct the GeoJSON URL for querying the feature layer
+    geojson_url = f"{url}/query?outFields=*&where=1%3D1&f=geojson"
+
     ## Construct list of source urls to account for max record counts
     sources = []
     offsets = [x for x in range(0, totalRecordCount, maxRecordCount)]
     for i in range(len(offsets)):
         start = offsets[i]
-        if offsets[i]+maxRecordCount-1 > totalRecordCount:
+        if offsets[i] + maxRecordCount > totalRecordCount:
             finish = totalRecordCount
-            maxRecordCount = totalRecordCount - offsets[i]+1
+            maxRecordCount = totalRecordCount - offsets[i]
         else:
-            finish = offsets[i]+maxRecordCount-1
+            finish = offsets[i] + maxRecordCount - 1
         source = {
             "title" : f"{start}-{finish}",
             "path": f"{geojson_url}&resultOffset={offsets[i]}&resultRecordCount={maxRecordCount}"
                  }
         sources.append(source)
 
-    firstTime = True
-    offset = 0
-    exceededLimit = True
+    # Fetch the GeoJSON data in chunks via source urls constructed above
+    features = []
     for i in range(len(sources)):
         print_bar(i, len(sources))
         # Request geojson for each source url
         if api_key == None:
-            r = requests.get(sources[i]['path'])
+            r = requests.get(sources[i]['path'], headers=headers)
         else:
-            r = requests.get(f"{sources[i]['path']}&key={api_key}")
+            r = requests.get(f"{sources[i]['path']}&key={api_key}", headers=headers)
         # Extract the GeoJSON from the API response
         try:
             result = r.json()
@@ -257,17 +306,22 @@ def get(resource_path, field_ids=None, api_key=None):
             print(f"CONTENTS OF REQUESTS {r.content}")
 
 
-        # Read this chunk of data into a GeoDataFrame
-        temp = gpd.GeoDataFrame.from_features(result["features"])
-        if firstTime:
-            # If this is the first chunk of data, create a permanent copy of the GeoDataFrame that we can append to
-            gdf = temp.copy()
-            firstTime = False
-        else:
-            # If this is not the first chunk, append to the permanent GeoDataFrame
-            gdf = pd.concat([gdf, temp], axis='index')
+        # Check if the request was successful
+        if 'error' in result:
+            print(f"Error fetching data: {result['error']['message']}")
+            raise RuntimeError
+        
+        # Check if the result contains features
+        if 'features' not in result:
+            print(f"No features found in the response. Check the URL or parameters.")
+            raise RuntimeError
+    
+        features.append(result)
 
-        # Increase the offset so that the next request fetches the next chunk of data
-        offset += maxRecordCount
+    # Convert GeoJSON features to GeoDataFrame
+    gdf = pd.concat([gpd.GeoDataFrame.from_features(geojson.FeatureCollection(x)) for x in features])
+    
+    # Set the coordinate reference system of the GeoDataFrame
+    gdf = gpd.GeoDataFrame(gdf, geometry='geometry', crs=CRS.from_wkt(wkt))
 
-    return(gdf.set_crs(f"epsg:{epsg}"))
+    return(gdf)
